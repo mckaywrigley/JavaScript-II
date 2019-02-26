2d72c94f2ce0f26b8f823db6c5954b1696b6f497
Mckay <mckaywrigley@gmail.com>
2019-02-26 10:21:10 -0700

    wrote closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,12 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let firstName = 'Mckay';
+function hello() {
+  let lastName = 'Wrigley';
+  console.log(`Hello ${firstName} ${lastName}`);
+}
 
+hello();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
